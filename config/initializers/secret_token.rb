04a82f8748ed5a5d797292a364d478822c7df1ce
Garrett Kelly <boontdustie@gmail.com>
2013-07-21 18:45:40 -0700

    rails 4 project for the lpfm-website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LpfmWebsite::Application.config.secret_key_base = '6ee845bcd9f5a09d162d9e919d21da4fd05bc3b57c9ad45cca06c33cd40fef1a6a11bebe6d1a770e4465c8a4c8c60dc2816db91ec0571867ee09b5ec629ec5bb'
