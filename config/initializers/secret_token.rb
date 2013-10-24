213d87a31be6aabd5e42a4c39367565faa54b0e2
Kenneth M. Piekarski <kenneth.m.piekarski@gmail.com>
2013-10-24 01:07:27 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HollowEarthRadio::Application.config.secret_key_base = '7b67404a7b1563c36648e53de07f7830fce450fe8002b3dbdc729fd1f636346d85459e0a1461161c2dbcd31b654398ae4e0aed653944ef3cbce1c498bfd09778'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HollowEarthRadio::Application.config.session_store :cookie_store, key: '_HollowEarthRadio_session'
