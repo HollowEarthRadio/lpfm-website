cc8332dc67ad94c91f4a6435e5c96cb66c6e4185
Kenneth M. Piekarski <kenneth.m.piekarski@gmail.com>
2013-11-03 16:02:04 -0800

    parsed out .scss, building uniform layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 HollowEarthRadio::Application.routes.draw do
-  get "home/index"
+  #get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
