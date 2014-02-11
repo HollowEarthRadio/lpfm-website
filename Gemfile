be87ba8f25dc1db66822a015c8a8b95fd0abd5d4
Garrett Kelly <boontdustie@gmail.com>
2014-02-10 21:34:55 -0800

    Trying something

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
+ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
+gem 'rails_12factor'
 
 group :development do
   gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ LpfmWebsite::Application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+  config.assets.debug = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
