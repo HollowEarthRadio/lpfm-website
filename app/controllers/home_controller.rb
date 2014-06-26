3d03485bd2e4fc80315e4caa19cde53b278c3dae
Chris Gual <btpnlsl@gmail.com>
2014-06-25 18:52:23 -0700

    Add twitter feed to sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   layout "default"
 
+  before_action :set_twitter_id
+
   def index
     @program = Program.random
     @events = Event.public.order('updated_at DESC')
@@ -13,5 +15,11 @@ class HomeController < ApplicationController
   def magma
     @magma_shows = MagmaShow.all.order("date ASC")
     @sponsors = Sponsor.all.where(year: Time.zone.now.year.to_s)
+  end
+
+  private
+
+  def set_twitter_id
+    @twitter_id = "hollowearth"
   end
 end
