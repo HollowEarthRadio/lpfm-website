64366844a78076ad3d161d6a6600eb2e2d3dbaa6
cgual <cgual@airtime.(none)>
2016-03-20 21:19:20 -0700

    Remove campaign splash page from front of site

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ LpfmWebsite::Application.routes.draw do
   get "/about", controller: "home", action: 'about'
   get "/contact", controller: "home", action: 'contact'
   get "/schedule", controller: "home", action: 'schedule'
-  #get "/donate", controller: "home", action: 'donate'
-  get "/donate", to: redirect("https://www.indiegogo.com/projects/hollow-earth-radio-finally-on-the-airwaves--2#/")
+  get "/donate", controller: "home", action: 'donate'
+  #get "/donate", to: redirect("https://www.indiegogo.com/projects/hollow-earth-radio-finally-on-the-airwaves--2#/")
   get "/volunteer", controller: "home", action: 'volunteer'
   get "/submissions", controller: "home", action: 'contribute'
   get "/faq", controller: "home", action: 'faq'
@@ -21,10 +21,10 @@ LpfmWebsite::Application.routes.draw do
   get '/theseaportbeat', controller: "home", action: "theseaportbeat"
   get "/magma" => redirect("http://www.magmafest.org")
   get '/splash', controller: "home", action: "splash"
-  get '/campaign', controller: "home", action: "campaign"
+  # get '/campaign', controller: "home", action: "campaign"
   get '/shirts', to: redirect('http://hollowearthradio.bandcamp.com/merch/')
   get '/listen', controller: "home", action: "listen"
 
-  #root to: 'home', action: 'index'
-  root to: 'home', action: 'campaign'
+  root to: 'home', action: 'index'
+  #root to: 'home', action: 'campaign'
 end
