e23f6840f020f33bbaac65f12e51991e4a38cefb
Christopher Gual <btpnlsl@gmail.com>
2018-03-29 09:38:02 -0700

    Add mobile detection in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,23 @@ class ApplicationController < ActionController::Base
       redirect_to "/admin/login"
     end
   end
+
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+    prepare_for_mobile if mobile_device?
+  end
+
+  def prepare_for_mobile
+    prepend_view_path Rails.root + 'app' + 'views_mobile'
+  end
+
+  def mobile_device?
+    if session[:mobile_override]
+      session[:mobile_override] == "1"
+    else
+      # Season this regexp to taste. I prefer to treat iPad as non-mobile.
+      (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/)
+    end
+  end
+  helper_method :mobile_device?
 end
