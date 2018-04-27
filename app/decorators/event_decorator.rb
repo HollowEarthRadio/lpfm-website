5447c14efcd07a1866110538bbd5f4daab857196
Garrett Kelly <garrettk@subpop.com>
2018-04-26 23:12:20 -0700

    Fixing events

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -1,4 +1,7 @@
 class EventDecorator < Draper::Decorator
+  decorates :event
+  delegate_all
+
   def hint_image
     if source.event_image.present?
       h.image_tag(source.event_image.url(:cropped))
