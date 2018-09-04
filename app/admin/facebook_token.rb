821641880dedd5d067402a94a6a885de9884c50f
Christopher Gual <btpnlsl@gmail.com>
2018-09-03 23:29:44 -0700

    Small changes for PR

--- app/admin/facebook_token.rb
+++ app/admin/facebook_token.rb
@@ -5,15 +5,16 @@ include ERB::Util
 ActiveAdmin.register_page "Facebook Token" do
   page_action(:store_token) do
     auth = request.env['omniauth.auth']
+    if !auth.nil?
+      # Sets 60 day auth token
+      oauth = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'])
+      new_access_info = oauth.exchange_access_token_info auth.credentials.token
 
-    # Sets 60 day auth token
-    oauth = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'])
-    new_access_info = oauth.exchange_access_token_info auth.credentials.token
+      new_access_token = new_access_info["access_token"]
+      new_access_expires_at = DateTime.now + new_access_info["expires"].to_i.seconds
 
-    new_access_token = new_access_info["access_token"]
-    new_access_expires_at = DateTime.now + new_access_info["expires"].to_i.seconds
-
-    FacebookToken.create(access_token: new_access_token, refresh_token: "");
+      FacebookToken.create(access_token: new_access_token, refresh_token: "");
+    end
 
     redirect_to "/admin/facebook_token"    
   end
