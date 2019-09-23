f977e525bec5d1652b5c4f8e97bd2bedd81afcfe
Chris Gual <btpnlsl@gmail.com>
2019-09-22 22:31:44 -0700

    Remove unused JS for airtime weekly schedule"

--- app/assets/javascripts/airtime.js
+++ app/assets/javascripts/airtime.js
@@ -7,15 +7,6 @@ ready = function() {
     text: { offline:"Offline", current:"Current Show", next:"Up Next" },
     updatePeriod: 600
   });
-
-  $("#scheduleTabs").airtimeWeekSchedule({
-    sourceDomain: HERDomain,
-    miscText: {time:"Time (PST)", programName:"Program Name", details:"Details", readMore:"Read More"},
-    updatePeriod: 600
-  });
-
-  var d = new Date().getDay();
-  $('#scheduleTabs').tabs({selected: (d === 0 ? 6 : d-1), fx: { opacity: 'toggle' }});
 }
 
 $(document).ready(ready);
